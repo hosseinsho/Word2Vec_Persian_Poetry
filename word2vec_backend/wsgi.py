cbe20fb4c08b0d0172bf7e9cdc723bed474a4322
hosseinsho <amiramiry125@gmail.com>
2022-05-18 22:48:24 +0430

    fix:  change rrrr to word2vec_backend

--- word2vec_backend/wsgi.py
+++ word2vec_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for rrrr project.
+WSGI config for word2vec_backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rrrr.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'word2vec_backend.settings')
 
 application = get_wsgi_application()

d41bae2cfd2edc80d6b752d0f27f7a3df378ffe6
hosseinsho <amiramiry125@gmail.com>
2022-05-18 22:35:31 +0430

    refact: replace django project directory

--- word2vec_backend/urls.py
+++ word2vec_backend/urls.py
@@ -1,4 +1,4 @@
-"""word2vec_backend URL Configuration
+"""rrrr URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
--- word2vec_backend/wsgi.py
+++ word2vec_backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for word2vec_backend project.
+WSGI config for rrrr project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'word2vec_backend.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rrrr.settings')
 
 application = get_wsgi_application()
